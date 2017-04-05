5a7fc2f8b68d3174bf2e4e28f0d0645c64f3cfa4
tuofeichen <tuofei.chen@gmail.com>
2017-04-05 16:48:36 -0500

    new cmu pcb

--- SmartTree_old/error.h
+++
@@ -1,8 +0,0 @@
-#ifndef ERROR_H
-#define ERROR_H
-
-#include "bmucell.h"
-
-bool displayError(volatile Cell cells[], int numel); // return true if big box error
-void setupPrintMinorError();
-#endif
